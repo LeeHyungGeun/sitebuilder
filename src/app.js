49565eac88c37bd3251fc91a4ae1f4195b8b1b08
leehyunggeun <lhg4031@gmail.com>
2019-05-05 23:51:09 +0800

    add component into app

--- src/app.js
+++ src/app.js
@@ -1,1 +1,3 @@
-alert('app.js')
+import Component from '@/lib/component'
+window.customElements.define('app-drawer', Component);
+document.querySelector('#root').appendChild(document.createElement('app-drawer'))
